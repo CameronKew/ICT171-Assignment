c3406ef1fb0eed49386425f3a52687fcc71c030f
CameronKew <35692486@student.murdoch.edu.au>
2026-05-27 17:37:01 +0800

    Create script.js

---
+++ Code/js/script.js
@@ -0,0 +1,13 @@
+const greeting = document.getElementById("greeting");
+
+const hour = new Date().getHours();
+
+if (hour < 12) {
+    greeting.innerText = "Good Morning";
+}
+else if (hour < 18) {
+    greeting.innerText = "Good Afternoon";
+}
+else {
+    greeting.innerText = "Good Evening";
+}
