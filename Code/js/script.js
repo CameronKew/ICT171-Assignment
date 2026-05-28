affebf3ffe72403fc8ecfd46e667ccee6422a451
CameronKew <35692486@student.murdoch.edu.au>
2026-05-28 17:04:46 +0800

    Update script.js

--- Code/js/script.js
+++ Code/js/script.js
@@ -1,13 +1,48 @@
 const greeting = document.getElementById("greeting");
 
-const hour = new Date().getHours();
+if (greeting) {
 
-if (hour < 12) {
-    greeting.innerText = "Good Morning";
+    const hour = new Date().getHours();
+
+    if (hour < 12) {
+        greeting.innerText = "Good Morning";
+    }
+    else if (hour < 18) {
+        greeting.innerText = "Good Afternoon";
+    }
+    else {
+        greeting.innerText = "Good Evening";
+    }
 }
-else if (hour < 18) {
-    greeting.innerText = "Good Afternoon";
+
+const toggleButton = document.getElementById("theme-toggle");
+
+if (localStorage.getItem("theme") === "light") {
+
+    document.body.classList.add("light-mode");
+
+    if (toggleButton) {
+        toggleButton.innerText = "Dark Mode";
+    }
 }
-else {
-    greeting.innerText = "Good Evening";
+
+if (toggleButton) {
+
+    toggleButton.addEventListener("click", function () {
+
+        document.body.classList.toggle("light-mode");
+
+        if (document.body.classList.contains("light-mode")) {
+
+            localStorage.setItem("theme", "light");
+
+            toggleButton.innerText = "Dark Mode";
+        }
+        else {
+
+            localStorage.setItem("theme", "dark");
+
+            toggleButton.innerText = "Light Mode";
+        }
+    });
 }
